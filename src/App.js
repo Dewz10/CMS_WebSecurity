0f1a66ad1bdcd5047613daf59ae92049dd8d22cb
autsadawut.kl <autsadawut.dew10@gmail.com>
2023-10-10 14:48:27 +0700

    update login

--- src/App.js
+++ src/App.js
@@ -1,37 +1,98 @@
-import React from "react";
-import { Routes, Route, Outlet, useLocation } from "react-router-dom";
+import React, { useState } from "react";
+import { Routes, Route, Outlet, useLocation, Navigate } from "react-router-dom";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 import News from "./views/students/News";
 import CheckStatus from "./views/students/CheckStatus";
 import Form from "./views/students/Form";
 import Login from "./views/login/Login";
+import FormCompany from "./views/company/FormCompany";
+import OpenRound from "./views/admin/OpenRound";
+import ManageRequest from "./views/admin/ManageRequest";
+import AddCompany from "./views/admin/AddCompany";
+import CheckStatusCompany from "./views/company/CheckStatusCompany";
 
 function App() {
+  const [authenticatedUser, setAuthenticatedUser] = useState(null);
+
+  const handleLogin = (username, password) => {
+    // Perform authentication logic here based on username and password.
+    // For example, you can use a switch statement or if-else conditions.
+    // Set the authenticated user based on the authentication result.
+    if (username === "stdtest001" && password === "stdtest001") {
+      setAuthenticatedUser("student");
+    } else if (username === "comtest001" && password === "comtest001") {
+      setAuthenticatedUser("company");
+    } else if (username === "admintest001" && password === "admintest001") {
+      setAuthenticatedUser("admin");
+    } else {
+      // Invalid credentials, handle accordingly.
+      setAuthenticatedUser(null);
+    }
+  };
+
   return (
     <div>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/"
-          element={
-            <div>
-              {window.location.pathname !== "/login" && window.location.pathname !== "/" && (
-                <>
+        <Route path="/login" element={<Login onLogin={handleLogin} />} />
+
+        {authenticatedUser === "student" && (
+          <Route
+            path="/"
+            element={
+              <>
+                <div>
                   <Header />
                   <Menu />
-                </>
-              )}
-              <Outlet />
-            </div>
-          }
-        >
-          {/* เส้นทางหลักเป็น /login */}
-          <Route index element={<Login />} />
-          <Route path="/news" element={<News />} />
-          <Route path="/register" element={<Form />} />
-          <Route path="/status" element={<CheckStatus />} />
-        </Route>
+                  <Outlet />
+                </div>
+              </>
+            }
+          >
+            <Route path="/news" element={<News />} />
+            <Route path="/register" element={<Form />} />
+            <Route path="/status" element={<CheckStatus />} />
+          </Route>
+        )}
+
+        {authenticatedUser === "company" && (
+          <Route
+            path="/"
+            element={
+              <>
+                <div>
+                  <Header />
+                  <Menu />
+                  <Outlet />
+                </div>
+              </>
+            }
+          >
+            <Route path="/comregisters" element={<FormCompany />} />
+            <Route path="/companystatus" element={<CheckStatusCompany />} />
+          </Route>
+        )}
+
+        {authenticatedUser === "admin" && (
+          <Route
+            path="/"
+            element={
+              <>
+                <div>
+                  <Header />
+                  <Menu />
+                  <Outlet />
+                </div>
+              </>
+            }
+          >
+            <Route path="/openrounds" element={<OpenRound />} />
+            <Route path="/managerequests" element={<ManageRequest />} />
+            <Route path="/addcompany" element={<AddCompany />} />
+          </Route>
+        )}
+
+        {authenticatedUser === null && <Navigate to="/login" />}
       </Routes>
     </div>
   );
--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Login() {
+function Login({ onLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [selectedRole, setSelectedRole] = useState("student");
+
+  const handleLogin = () => {
+    // Call the onLogin function and pass the entered username and password.
+    onLogin(username, password, selectedRole);
+  };
+
   return (
     <div style={{ display: "flex", justifyContent: "center", marginTop: 150 }}>
       <div className="login-box">
@@ -12,45 +21,60 @@ function Login() {
           </div>
           <div className="card-body">
             <p className="login-box-msg">Sign in to start your session</p>
-            <form action="/" method="post">
-              <div className="input-group mb-3">
-                <input
-                  type="email"
-                  className="form-control"
-                  placeholder="Email"
-                />
-                <div className="input-group-append">
-                  <div className="input-group-text">
-                    <span className="fas fa-envelope" />
-                  </div>
+            <div className="input-group mb-3">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <div className="input-group-append">
+                <div className="input-group-text">
+                  <span className="fas fa-envelope" />
                 </div>
               </div>
-              <div className="input-group mb-3">
-                <input
-                  type="password"
-                  className="form-control"
-                  placeholder="Password"
-                />
-                <div className="input-group-append">
-                  <div className="input-group-text">
-                    <span className="fas fa-lock" />
-                  </div>
+            </div>
+            <div className="input-group mb-3">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <div className="input-group-append">
+                <div className="input-group-text">
+                  <span className="fas fa-lock" />
                 </div>
               </div>
-              <div className="row">
-                <div className="col-8">
-                  <div className="icheck-primary">
-                    <input type="checkbox" id="remember" />
-                    <label htmlFor="remember">Remember Me</label>
-                  </div>
-                </div>
-                <div className="col-4">
-                  <a href="/news" className="btn btn-primary btn-block">
-                    Sign In
-                  </a>
+            </div>
+            <div className="mb-3">
+              <label>Select Role:</label>
+              <select
+                className="form-control"
+                onChange={(e) => setSelectedRole(e.target.value)}
+                value={selectedRole}
+              >
+                <option value="student">Student</option>
+                <option value="company">Company</option>
+                <option value="admin">Admin</option>
+              </select>
+            </div>
+            <div className="row">
+              <div className="col-8">
+                <div className="icheck-primary">
+                  <input type="checkbox" id="remember" />
+                  <label htmlFor="remember">Remember Me</label>
                 </div>
               </div>
-            </form>
+              <div className="col-4">
+                <button
+                  className="btn btn-primary btn-block"
+                  onClick={handleLogin}
+                >
+                  Sign In
+                </button>
+              </div>
+            </div>
             <p className="mb-0">
               <a href="register.html" className="text-center">
                 Register a new membership
