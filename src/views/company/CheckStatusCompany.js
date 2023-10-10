2f6303789f2db427c80519b56494edd35f737d68
autsadawut.kl <autsadawut.dew10@gmail.com>
2023-10-10 14:43:13 +0700

    ui company

---
+++ src/views/company/CheckStatusCompany.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CheckStatusCompany() {
+  return (
+    <div>CheckStatusCompany</div>
+  )
+}
+
+export default CheckStatusCompany
---
+++ src/views/company/FormCompany.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FormCompany() {
+  return (
+    <div>FormCompany</div>
+  )
+}
+
+export default FormCompany
