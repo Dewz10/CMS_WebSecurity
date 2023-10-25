e972ea0211be45c759633ecba4729b9bf1f88224
thitiphon.s <colonp.pth@gmail.com>
2023-10-25 10:58:57 +0700

    get request

--- src/services/internshipService.js
+++ src/services/internshipService.js
@@ -1,14 +1,14 @@
 import axios from 'axios'
 
 const API_BASE_URL = 'http://localhost:3000/internship'
-const token = localStorage.getItem('access_token')
+//const token = localStorage.getItem('access_token')
 
 
 const getAllRequest = async () => {
     try {
         const response = await axios.get(`${API_BASE_URL}/request/user`, {
             headers: {
-                Authorization: 'Bearer '+token,
+                Authorization: 'Bearer '+localStorage.getItem('access_token'),
             }
         });
         return response.data;
