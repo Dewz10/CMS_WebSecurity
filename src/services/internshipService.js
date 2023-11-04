9344b2207758263fbea324c215cab722cb48383a
thitiphon.s <colonp.pth@gmail.com>
2023-11-04 22:59:11 +0700

    http to https service

--- src/services/internshipService.js
+++ src/services/internshipService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:3000/internship'
+const API_BASE_URL = 'https://localhost:3000/internship'
 //const token = localStorage.getItem('access_token')
 
 
